efdd3ff3a83218ab8eb55debf40410aa34dbf6d7
Hervé Guétin <herve.guetin@gmail.com>
2023-12-07 09:26:23 +0100

    Algolia - Indices config update

--- web/app/Services/Algolia/config/indices.php
+++ web/app/Services/Algolia/config/indices.php
@@ -2,14 +2,19 @@
 /**
  * @author Hervé Guétin <www.linkedin.com/in/herveguetin>
  */
+
+use App\Services\Algolia\Index\Collections;
+use App\Services\Algolia\Index\Products;
+
 return [
     // Products
-    'products' => [
+    Products::INDEX_CODE => [
         'settings' => [
             'parameters' => [
                 'attributesForFaceting' => ['collections']
             ]
         ]
-    ]
+    ],
     // Collections
+    Collections::INDEX_CODE => [],
 ];
